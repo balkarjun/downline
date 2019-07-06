2ccf203ead901a2d8f37dd2679cb7a6a9727c297
jarbun <jarbun306@gmail.com>
2019-07-07 00:14:25 +0530

    Add logic to get video and audio formats

--- src/api.js
+++ src/api.js
@@ -20,6 +20,38 @@ function createDownloadable(data) {
   console.log(metadata);
 }
 
+function getFormats(rawFormats) {
+  let formats = { video: [], audio: [] };
+  let audioSeen = new Set();
+  let videoSeen = new Set();
+
+  rawFormats.forEach(format => {
+    const type = getMediaType(format);
+    const { abr, height, format_id } = format;
+
+    if (type === MediaType.AUDIO && abr && !audioSeen.has(abr)) {
+      formats.audio.push({
+        quality: abr,
+        code: `bestaudio[abr<=${abr}]`
+      })
+      audioSeen.add(abr);
+    } else if (type === MediaType.VIDEO_ONLY && height && !videoSeen.has(height)) {
+      formats.video.push({
+        quality: height,
+        code: `bestvideo[height<=${height}]+bestaudio/best[height<=${height}]`
+      })
+      videoSeen.add(height);
+    } else if (type === MediaType.VIDEO && !videoSeen.has(height || format_id)) {
+      formats.video.push({
+        quality: height || format_id,
+        code: format_id
+      })
+      videoSeen.add(height || format_id);
+    }
+  });
+  return formats;
+}
+
 function getMediaType(format) {
   const { vcodec, acodec, height, width } = format;
 
