84c6c61d2c2cefe34ba418d7dde151b379670195
jarbun <jarbun306@gmail.com>
2019-07-16 15:44:20 +0530

    Modify InputBar component to new design

--- src/components/InputBar.vue
+++ src/components/InputBar.vue
@@ -1,40 +1,23 @@
 <template>
   <div>
-    <div v-if="activeInput===0" id="single-link">
-      <input type="text" v-model="inputs.single" placeholder="Type or paste a link">
-      <div class="buttons">
-        <SwapButton @next="nextInput" />
-      </div>
-      <button @click="addSingleLink">Add Link</button>
-    </div>
-    <div v-else-if="activeInput===1" id="multiple-links">
-      <textarea ref="textarea" @keydown.ctrl.86="isPasted = true" @input="updateValue($event)" placeholder="Type or paste one link per line"></textarea>
-      <div class="buttons">
-        <SwapButton class="swap-button" @next="nextInput" />
-        <button @click="addMultipleLinks" class="add-links">
-          <img src="../assets/icons/add.svg">
-        </button>
-      </div>
-    </div>
-    <div v-else></div>
+    <textarea 
+      ref="textarea" 
+      @keydown.ctrl.86="isPasted = true" 
+      @input="updateValue($event)" 
+      placeholder="Enter one link per line"
+    ></textarea>
+    <button @click="addLinks">
+      <img src="../assets/icons/add.svg">
+    </button>
   </div>
 </template>
 
 <script>
-import SwapButton from './SwapButton.vue';
-
 export default {
   name: 'input-bar',
-  components: {
-    SwapButton
-  },
   data() {
     return {
-      activeInput: 1,
-      inputs: {
-        single: '',
-        multiple: ''
-      },
+      value: '',
       isPasted: false
     }
   },
@@ -44,50 +27,40 @@ export default {
       this.isPasted = false;
       event.target.value = newValue;
     },
-    nextInput() {
-      this.activeInput = (this.activeInput + 1) % 2;
-    },
-    addSingleLink() {
-      if (this.inputs.single.trim() !== '') {
-        console.log(this.inputs.single.trim());
-      }
-      this.inputs.single = '';
-    },
-    addMultipleLinks() {
+    addLinks() {
       const value = this.$refs.textarea.value.trim();
       if (value !== '') {
-        console.log(value.split('\n'));
+        this.value = value.split('\n');
       }
       this.$refs.textarea.value = '';
+      console.log(this.value);
     }
   }
 };
 </script>
 
 <style scoped>
-input {
-  box-sizing: border-box;
-  font-size: 15px;
-  font-family: Rubik;
-}
-
 textarea {
-  height: 100px;
-  width: 100%;
-  border-radius: 5px 0 0 5px;
-  padding: 12px 0 0 16px;
-  box-sizing: border-box;
   font-family: Rubik;
   font-size: 15px;
+  height: 100px;
+  width: calc(100% - 32px);
+  box-sizing: border-box;
+  border-radius: 5px;
+  margin: 0 16px;
+  padding: 12px 0 0 16px;
+  border: 1px solid lightgray;
+  outline: none;
   white-space: nowrap;
   resize: none;
-  border: 1px solid lightgray;
-  border-right: none;
+}
+textarea:focus {
+  border: 1px solid lightblue;
 }
 /* Width */
 textarea::-webkit-scrollbar {
   width: 3px;
-  height: 3px;
+  height: 0;
 }
 /* Handle */
 textarea::-webkit-scrollbar-thumb {
@@ -95,67 +68,19 @@ textarea::-webkit-scrollbar-thumb {
   border-radius: 5px;
 }
 
-.buttons {
-  display: flex;
-}
-
-#single-link {
-  display: flex;
-  margin: 0 16px;
-}
-
-#single-link .buttons {
-  padding: 0 8px;
-  border: 1px solid lightgray;
-  border-left: none;
-  border-right: none;
-}
-
-#single-link input {
-  height: 44px;
-  padding-left: 16px;
-  flex-grow: 1;
-  border-radius: 5px 0 0 5px;
-  border: 1px solid lightgray;
-  border-right: none;
-}
-
-#single-link button {
-  padding: 0 16px;
-  border-radius: 0 5px 5px 0;
-}
-
-#multiple-links {
-  display: flex;
-  margin: 0 16px;
-}
-
-#multiple-links .buttons {
-  width: 52px;
-  border: 1px solid lightgray;
-  border-left: none;
-  border-radius: 0 5px 5px 0;
-  flex-direction: column;
-  justify-content: space-between;
-  box-sizing: border-box;
-}
-
-#multiple-links .swap-button {
-  margin: 8px 8px 0 0;
-  align-self: flex-end;
-}
-
-#multiple-links .add-links {
+button {
   width: 44px;
   height: 44px;
-  border-radius: 5px;
-  margin: 0 8px 8px 0;
+  border-radius: 50%;
   display: flex;
   justify-content: center;
+  margin: 0 auto;
+  position: relative;
+  bottom: 26px;
 }
 
-#multiple-links .add-links img {
-  width: 30px;
-  height: 30px;
+button img {
+  width: 28px;
+  height: 28px;
 }
 </style>
