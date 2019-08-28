e77fcf609f5d4faacf336eff64abb5645ac40bbf
jarbun <jarbun306@gmail.com>
2019-08-28 23:30:26 +0530

    Add menu dialog

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="titlebar">
-    <div class="left">
-      <img src="../assets/icons/more_horiz.svg">
-    </div>
+    <OnClickOutside :do="closeDialog">
+      <div class="left">
+        <img @click="openDialog" src="../assets/icons/more_horiz.svg">
+        <div class="dialog" v-if="isOpen">
+          <p>Settings</p>
+          <p>About</p>
+        </div>
+      </div>
+    </OnClickOutside>
     <div class="right">
       <span @click="minimize">
         <img src="../assets/icons/minimize.svg">
@@ -15,11 +21,27 @@
 </template>
 
 <script>
+import OnClickOutside from './OnClickOutside.vue';
+
 const { remote } = window.require('electron');
 
 export default {
   name: 'title-bar',
+  components: {
+    OnClickOutside,
+  },
+  data() {
+    return {
+      isOpen: false
+    }
+  },
   methods: {
+    openDialog() {
+      this.isOpen = true;
+    },
+    closeDialog() {
+      this.isOpen = false;
+    },
     close() {
       remote.getCurrentWindow().close();
     },
@@ -53,5 +75,26 @@ export default {
   padding: 0 12px;
   display: flex;
   cursor: pointer;
+}
+
+.dialog {
+  width: 120px;
+  position: absolute;
+  left: 16px;
+  top: 12px;
+  z-index: 1;
+  border-radius: 5px;
+  background-color: white;
+  padding: 4px 0;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, .2);
+}
+
+.dialog p {
+  padding: 6px 0 6px 16px;
+  cursor: pointer;
+}
+
+.dialog p:hover {
+  background-color: whitesmoke;
 }
 </style>
