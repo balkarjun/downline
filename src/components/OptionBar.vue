067303212d2739d1bcd80f9e4c4f7ed35283d72e
jarbun <jarbun306@gmail.com>
2019-09-03 20:19:46 +0530

    Refactor optionbar

--- src/components/OptionBar.vue
+++ src/components/OptionBar.vue
@@ -2,9 +2,11 @@
   <div>
     <button>Download All</button>
     
-    <p v-if="count > 0" class="count">
+    <p v-show="count > 0">
       {{ count }}
-      <span class="light">{{ `Item${count > 1 ? 's': ''}` }}</span>
+      <span class="light">
+        Item{{ count > 1 ? 's' : '' }}
+      </span>
     </p>
   </div>
 </template>
@@ -19,19 +21,15 @@ export default {
 <style scoped>
 div {
   height: 30px;
-  width: calc(100% - 32px);
-  margin: 12px 16px 6px 16px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
+  margin: 12px 32px 6px 16px;
 }
 
 button {
+  height: 30px;
   width: 120px;
-  border-radius: 5px;
-}
-
-.count {
-  align-self: center;
-  margin-right: 16px;
+  border-radius: 4px;
 }
 </style>
