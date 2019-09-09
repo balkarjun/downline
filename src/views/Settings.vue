4027d699ca900bda7cbb0e033456cd3034d42b52
jarbun <jarbun306@gmail.com>
2019-09-09 14:18:25 +0530

    Add audio format setting with dialog

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -22,7 +22,7 @@
     </section>
     <section class="filename">
       <label>Filename</label>
-      <div class="item format">
+      <div class="item filename-format">
         <p class="title">Format</p>
         <div class="value">
           <button 
@@ -42,16 +42,41 @@
         <Checkbox v-model="isAutonumber"/>
       </div>
     </section>
+    <section class="format">
+      <label>Format</label>
+      <div class="item audio-format">
+        <p class="title">Audio Format</p>
+        <OnClickOutside :do="closeAudioDialog">
+          <div class="value">
+            <div ref="audioRef"></div>
+            <div @click="openAudioDialog" class="active-audio">
+              <p>{{ audioFormat }}</p>
+            </div>
+            <div ref="audioDialog" v-if="isAudioOpen" class="dialog">
+              <p
+                v-for="(format, index) in audioFormats"
+                :key="index"
+                :class="{active: index === audioIndex}"
+                @click="selectAudio(index)"
+              >{{ format }}</p>
+            </div>
+          </div>
+        </OnClickOutside>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import Checkbox from '../components/Checkbox.vue';
+import OnClickOutside from '../components/OnClickOutside.vue';
+import Popper from 'popper.js';
 
 export default {
   name: 'settings',
   components: {
-   Checkbox 
+   Checkbox,
+   OnClickOutside
   },
   data() {
     return {
@@ -61,11 +86,47 @@ export default {
         { key: '%(id)s.%(ext)s', name: 'ID', active: false },
         { key: '%(title)s-%(id)s.%(ext)s', name: 'Title + ID', active: false },
       ],
+      audioFormats: ['mp3', 'wav', 'aac', 'flac', 'opus', 'vorbis'],
+      audioIndex: 0,
+      isAudioOpen: false,
       isAscii: false,
       isAutonumber: false,
     }
   },
+  computed: {
+    audioFormat() {
+      return this.audioFormats[this.audioIndex];
+    }
+  },
   methods: {
+    openAudioDialog() {
+      this.isAudioOpen = true;
+      this.$nextTick(() => {
+        this.setupPopper();
+        const activeElement = this.$refs.audioDialog.children[this.audioIndex];
+        activeElement.scrollIntoView({ block: 'center' });
+      });
+    },
+    setupPopper() {
+      new Popper(this.$refs.audioRef, this.$refs.audioDialog, {
+        placement: 'bottom-start',
+        modifiers: {
+          flip: {
+            enabled: false
+          },
+          computeStyle: {
+            gpuAcceleration: false
+          }
+        }
+      });
+    },
+    closeAudioDialog() {
+      this.isAudioOpen = false;
+    },
+    selectAudio(index) {
+      this.audioIndex = index;
+      this.closeAudioDialog();
+    },
     setActiveFilenameFormat(index) {
       this.filenameFormats.forEach(x => x.active = false);
       this.filenameFormats[index].active = true;
@@ -83,6 +144,8 @@ export default {
 #settings-page {
   padding-top: 16px;
   padding-left: 24px;
+  height: 100vh;
+  overflow: scroll;
 }
 
 label {
@@ -133,7 +196,7 @@ label {
   margin-top: 24px;
 }
 
-.format .value button {
+.filename-format .value button {
   background-color: white;
   height: 26px;
   padding: 0 12px;
@@ -142,8 +205,54 @@ label {
   margin-right: 8px;
 }
 
-.format .value button.active {
+.filename-format .value button.active {
   background-color: lightgray;
   border-color: transparent;
+}
+
+.format {
+  margin-top: 24px;
+}
+
+.active-audio {
+  display: flex;
+  align-items: center;
+  height: 26px;
+  padding: 0 12px;
+  border: 1px solid lightgray;
+  background-color: white;
+  border-radius: 4px;
+  cursor: pointer;
+  box-sizing: border-box;
+}
+
+.dialog {
+  background-color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 4px 0;
+  width: 110px;
+  max-height: 148px;
+  overflow: scroll;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, .2);
+  box-sizing: border-box;
+}
+
+.dialog::-webkit-scrollbar {
+  width: 2px;
+}
+
+.dialog p {
+  padding-left: 16px;
+  line-height: 28px;
+  cursor: pointer;
+}
+
+.dialog p:hover {
+  background-color: whitesmoke;
+}
+
+.dialog p.active {
+  background-color: lightgray;
 }
 </style>
