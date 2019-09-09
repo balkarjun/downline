f97d31b661089eeea700e31264948f48f1ab0e83
jarbun <jarbun306@gmail.com>
2019-09-09 10:36:32 +0530

    Add checkbox component

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="toggle" :class="{checked: value}">
+    <img v-if="value" src="../assets/icons/check.svg">
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'checkbox',
+  props: ['value'],
+  methods: {
+    toggle() {
+      this.$emit('input', !this.value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  width: 20px;
+  height: 20px;
+  padding: 0;
+  border-radius: 4px;
+  border: 1px solid lightgray;
+  display: flex;
+  justify-content: center;
+  background-color: white;
+}
+
+.checked {
+  background-color: gray;
+}
+</style>
