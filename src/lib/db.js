a56087b29e93ca3700b31a1e59374102b258babc
jarbun <jarbun306@gmail.com>
2019-09-18 18:49:17 +0530

    Rename store to db

---
+++ src/lib/db.js
@@ -0,0 +1,43 @@
+const electron = require('electron');
+const { join } = require('path');
+const fs = require('fs');
+
+const defaults = {
+  downloadLocation: '../',
+  simultaneous: 5,
+  filenameFormats: [
+    { key: '%(title)s.%(ext)s', name: 'Title' },
+    { key: '%(id)s.%(ext)s', name: 'ID' },
+    { key: '%(title)s-%(id)s.%(ext)s', name: 'Title + ID' }
+  ],
+  filenameIndex: 0,
+  audioFormats: ['mp3', 'wav', 'aac', 'flac', 'opus', 'vorbis'],
+  audioIndex: 0,
+  videoFormats: ['default', 'mp4', 'webm', 'mkv'],
+  videoIndex: 0,
+  ascii: false,
+  autonumber: false
+};
+
+const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+const path = join(userDataPath, 'settings.json');
+
+let data = defaults;
+if (fs.existsSync(path)) {
+  data = JSON.parse(fs.readFileSync(path));
+}
+
+function get(key) {
+  return data[key];
+}
+
+function set(key, val) {
+  data[key] = val;
+  console.log(`[set] ${key}:${val}`);
+}
+
+function update() {
+  fs.writeFileSync(path, JSON.stringify(data));
+}
+
+export default { get, set, update };
