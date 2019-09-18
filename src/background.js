1bddbacb0f7a91d4af5e0e17c8607a42ca4eddb0
jarbun <jarbun306@gmail.com>
2019-09-18 18:50:22 +0530

    Add logic from electron builder vue plugin

--- src/background.js
+++ src/background.js
@@ -1,10 +1,22 @@
-/* Electron Main Process */
-const { app, BrowserWindow } = require('electron');
+'use strict';
 
-let win = null;
-let url = 'http://localhost:8080/';
+import { app, protocol, BrowserWindow } from 'electron';
+import {
+  createProtocol,
+  installVueDevtools
+} from 'vue-cli-plugin-electron-builder/lib';
 
-app.on('ready', () => {
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+let win;
+
+// Scheme must be registered before the app is ready
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'app', privileges: { secure: true, standard: true } }
+]);
+
+function createWindow() {
+  // Create the browser window
   win = new BrowserWindow({
     width: 850,
     height: 520,
@@ -15,10 +27,63 @@ app.on('ready', () => {
     }
   });
 
-  win.webContents.openDevTools();
-  win.loadURL(url);
-  win.once('ready-to-show', () => win.show());
-  win.on('closed', () => win = null);
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+    if (!process.env.IS_TEST) win.webContents.openDevTools();
+  } else {
+    createProtocol('app');
+    // Load the index.html when not in development
+    win.loadURL('app://./index.html');
+  }
+
+  win.once('ready-to-show', win.show);
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on('window-all-closed', () => app.quit());
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open
+  if (win === null) {
+    createWindow();
+  }
+});
+
+app.on('ready', async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    // Install Vue Devtools
+    try {
+      await installVueDevtools();
+    } catch (e) {
+      console.error('Vue Devtools failed to install:', e.toString());
+    }
+  }
+  createWindow();
+});
+
+// Exit cleanly on request from parent process in development mode
+if (isDevelopment) {
+  if (process.platform === 'win32') {
+    process.on('message', data => {
+      if (data === 'graceful-exit') {
+        app.quit();
+      }
+    });
+  } else {
+    process.on('SIGTERM', () => {
+      app.quit();
+    });
+  }
+}
