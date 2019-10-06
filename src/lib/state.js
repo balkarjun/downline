8abf502606d699fab7020335b30c36e26172f0c5
jarbun <jarbun306@gmail.com>
2019-10-06 15:31:01 +0530

    Add methods to check state

--- src/lib/state.js
+++ src/lib/state.js
@@ -1,9 +1,18 @@
-export default {
+const states = {
   STOPPED: 0,
+  isStopped: x => x === states.STOPPED,
   STARTING: 1,
+  isStarting: x => x === states.STARTING,
   DOWNLOADING: 2,
+  isDownloading: x => x === states.DOWNLOADING,
   PROCESSING: 3,
+  isProcessing: x => x === states.PROCESSING,
   PAUSED: 4,
+  isPaused: x => x === states.PAUSED,
   QUEUED: 5,
-  COMPLETED: 6
+  isQueued: x => x === states.QUEUED,
+  COMPLETED: 6,
+  isCompleted: x => x === states.COMPLETED
 };
+
+export default states;
